1ad15aa65b1238386e2d134d2f19ca6d54d0a296
Angel Cochachez <acochachez@gmail.com>
2020-11-05 01:34:41 -0800

    added the rest of the navbar link included Register and Sign in

--- finance-data/src/components/NavBar/NavBarElements.js
+++ finance-data/src/components/NavBar/NavBarElements.js
@@ -29,7 +29,7 @@ padding:0 24px;
 max-width:1100px
 `;
 export const NavLogo = styled(LinkR)`
-color:red;
+color:#fff;
 justify-self:flex-start;
 cursor:pointer;
 font-size:1.5rem;
@@ -51,7 +51,7 @@ display:none;
     transform:translate(-100%, 60%);
     font-size:1.8rem;
     cursor:pointer;
-
+    color: #fff;
 }
 `
 export const NavMenu = styled.ul`
@@ -79,5 +79,33 @@ cursor: pointer;
 
 &.active{
     border-bottom:3px solid #01bf71;
+}
+`;
+
+export const NavBtn = styled.nav `
+display:flex;
+align-items:center;
+
+@media screen and (max-width:760px){
+    display:none;
+}
+`
+export const NavBtnLink=styled(LinkR)`
+border-radius:50px;
+background:#01bf71;
+white-space:nowrap;
+padding:10px 22px;
+color:#010606;
+font-size:16px;
+outline:none;
+border:none;
+cursor: pointer;
+transition: all 0.2s ease-in-out;
+text-decoration:none;
+
+&:hover{
+    transition:all 0.2s ease-in-out;
+    background:#fff;
+    color:#010606;
 }
 `
--- finance-data/src/components/NavBar/index.js
+++ finance-data/src/components/NavBar/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {FaBars} from 'react-icons/fa'
-import {Nav, NavbarContainer, NavLogo,MobileIcon, NavMenu, NavItem, NavLinks} from './NavBarElements';
+import {Nav, NavbarContainer, NavLogo, MobileIcon, NavMenu, NavItem, NavLinks, NavBtn, NavBtnLink} from './NavBarElements';
 
 const Navbar =()=>{
     return(
@@ -17,8 +17,19 @@ const Navbar =()=>{
                     <NavItem>
                         <NavLinks to="product">Product</NavLinks>
                     </NavItem>
+                    <NavItem>
+                        <NavLinks to="docs">Docs</NavLinks>
+                    </NavItem>
+                    <NavItem>
+                        <NavLinks to="company">Company</NavLinks>
+                    </NavItem>
+                    <NavItem>
+                        <NavLinks to="register">Register</NavLinks>
+                    </NavItem>
                 </NavMenu>
-
+                <NavBtn>
+                    <NavBtnLink to="/signin">Sign In</NavBtnLink>
+                </NavBtn>
             </NavbarContainer>
         </Nav>
        </>
