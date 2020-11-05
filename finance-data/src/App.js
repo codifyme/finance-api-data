8c72dc387524bd865f0154f69b74f9e849993a53
Angel Cochachez <acochachez@gmail.com>
2020-11-05 00:30:22 -0800

    Added component files of index.js and navbar.js

--- finance-data/src/App.js
+++ finance-data/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/NavBar';
+import {BrowserRouter as Router} from 'react-router-dom'
 
 function App() {
   return (
-    <div>
+    <Router>
       <Navbar />
-    </div>
+    </Router>
 
   );
 }
--- finance-data/src/components/NavBar/index.js
+++ finance-data/src/components/NavBar/index.js
@@ -1,10 +1,26 @@
-import React from 'react'
+import React from 'react';
+import {FaBars} from 'react-icons/fa'
+import {Nav, NavbarContainer, NavLogo,MobileIcon, NavMenu, NavItem, NavLinks} from './NavBarElements';
 
 const Navbar =()=>{
     return(
-       <>
-        
+        <>
+        <Nav>
+            <NavbarContainer>
+                <NavLogo to='/'>
+                    Ziggurat
+                </NavLogo>
+                <MobileIcon>
+                    <FaBars />
+                </MobileIcon>
+                <NavMenu>
+                    <NavItem>
+                        <NavLinks to="product">Product</NavLinks>
+                    </NavItem>
+                </NavMenu>
 
+            </NavbarContainer>
+        </Nav>
        </>
     )
 }
