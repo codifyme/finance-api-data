207b59c0811dff77835307ed15de0b3207eafe68
Angel Cochachez <acochachez@gmail.com>
2020-11-05 21:51:42 -0800

    Added stylesheet, buttonselements and index.js

--- finance-data/src/components/HeroSection/index.js
+++ finance-data/src/components/HeroSection/index.js
@@ -1,19 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Video from '../../videos/video.mp4';
-import {HeroContainer, HeroBg, VideoBg} from './HeroElements';
+import {Button} from '../ButtonElement';
+import {HeroContainer, HeroBg, VideoBg, HeroContent,HeroH1, HeroP, HeroBtnWrapper,ArrowForward, ArrowRight} from './HeroElements';
 
 const HeroSection = () => {
+    const [hover, setHover]=useState(false)
+    const onHover=()=>{
+        setHover(!hover)        
+    }
+
   return (
     <HeroContainer>
       <HeroBg>
           <VideoBg autoPlay loop muted src={Video} type='video/mp4'/>
       </HeroBg>
       <HeroContent>
-          <HeroH1>Build and Explore our finance Database API</HeroH1>
-          <HeroP>The largest finance Database</HeroP>
+          <HeroH1>Build and Explore our finance Database API Docs, The largest finance Database</HeroH1>
           <HeroP>Sign up for a new account today and receive $250 in credit towards your next payment. </HeroP>
           <HeroBtnWrapper>
-              <Button to="register">
+              <Button to="register" onMouseEnter={onHover} onMouseLeave={onHover}
+              primary='true' dark='true'
+              >
                   Get started {hover ? <ArrowForward /> :<ArrowRight/>}
               </Button>
           </HeroBtnWrapper>
