1a38be841b64a420f358be74c549f401070589a5
Angel Cochachez <acochachez@gmail.com>
2020-11-11 16:09:03 -0800

    Fixed service layout and added an update on the description

--- finance-data/src/components/Service/ServicesElements.js
+++ finance-data/src/components/Service/ServicesElements.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 
 export const ServicesContainer = styled.div`
-    height:800px;
+    height:900px;
     display:flex;
     flex-direction:column;
     justify-content:center;
@@ -39,13 +39,13 @@ export const ServicesWrapper=styled.div`
 
     export const ServicesCard = styled.div`
     background:#fff;
-    display:flex;
+    
     flex-direction:column;
     justify-content:flex-start;
     align-items:center;
     border-radius:10px;
-    max-height:340px;
-    padding:30px;
+    max-height:440px;
+    padding:40px;
     box-shadow: 0 1px 3px rgba(0,0,0,0.2);
     transition:all 0.2s ease-in-out;
 
@@ -73,6 +73,7 @@ export const ServicesWrapper=styled.div`
 export const ServicesH2 = styled.h2`
 font-size:1rem;
 margin-bottom:10px;
+text-align:center;
 
 `
 export const ServicesP=styled.p`
--- finance-data/src/components/Service/index.js
+++ finance-data/src/components/Service/index.js
@@ -14,32 +14,40 @@ const Services = () => {
       <ServicesWrapper>
           <ServicesCard>
               <ServicesIcon src={Icon1}/>
-              <ServicesH2>
-                  ENTERPRISE
-              </ServicesH2>
-              <ServicesP>
-                  Pay-As-You-Go pricing based on what your business needs
-              </ServicesP>
+              <ServicesH2>ENTERPRISE</ServicesH2>
+              <ServicesP>Pay-As-You-Go pricing based on what your business needs</ServicesP>
+              <ServicesP>Longer Data Retention</ServicesP>
+              <ServicesP>Higher API Call Rates</ServicesP>
+              <ServicesP>Scalable Incoming Data</ServicesP>
+              <ServicesP>Multiple User Accounts</ServicesP>
+              <ServicesP>Service-Level Agreement</ServicesP>
+              <ServicesP>Enterprise Support</ServicesP>
           </ServicesCard>
 
           <ServicesCard>
               <ServicesIcon src={Icon2}/>
-              <ServicesH2>
-                  FOR TEAMS
-              </ServicesH2>
-              <ServicesP>
-                  $29/month
-              </ServicesP>
+              <ServicesH2>FOR TEAMS</ServicesH2>
+              <ServicesP>$29/month</ServicesP>
+              <ServicesP>Unlimited Devices</ServicesP>
+              <ServicesP>Unlimited Visualizations</ServicesP>
+              <ServicesP>180 API Call/min</ServicesP>
+              <ServicesP>Real-Time Expressions</ServicesP>
+              <ServicesP>Sharing/Non-Enterprise-Embeds</ServicesP>
+              <ServicesP>Unlimited Visualizations</ServicesP>
+              <ServicesP>180 API Calls/min</ServicesP>
+              <ServicesP>Email Support</ServicesP>
           </ServicesCard>
 
           <ServicesCard>
               <ServicesIcon src={Icon3}/>
               <ServicesH2>
-                  FOR INDIVIDUALS
+                  FOR INDIVIDUAL
               </ServicesH2>
-              <ServicesP>
-                  $10/month, billed monthly
-              </ServicesP>
+              <ServicesP>$10/month, billed monthly</ServicesP>
+              <ServicesP>Unlimited Devices</ServicesP>
+              <ServicesP>Unlimited Visualizations</ServicesP>
+              <ServicesP>Individual account for personal project</ServicesP>
+              <ServicesP>90 API Calls/Min</ServicesP>
           </ServicesCard>
 
           <ServicesCard>
@@ -50,6 +58,12 @@ const Services = () => {
               <ServicesP>
                   FREE for active students
               </ServicesP>
+              <ServicesP>Unlimited Devices</ServicesP>
+              <ServicesP>Unlimited Visualizations</ServicesP>
+              <ServicesP>2-Months Data Retention</ServicesP>
+              <ServicesP>60 API Calls/min</ServicesP>
+              <ServicesP>1M Incoming Data Points</ServicesP>
+              <ServicesP>With a.edu current email address</ServicesP>
           </ServicesCard>
 
       </ServicesWrapper>
